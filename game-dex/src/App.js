3866e64cd0829551ac9cb3907a4907861dba08d6
lucasckrug <lucasckrug@hotmail.com>
2025-05-29 21:26:06 -0300

    esqueci minha senha e mudar senha funcionando

--- game-dex/src/App.js
+++ game-dex/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import Home from './pages/home';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
+import ForgotPassword from './pages/forgotpassword';
+import ResetPassword from './pages/resetpassword';
 import { AuthProvider } from './components/AuthContext';
 import MyGames from './pages/Mygames';
 import Infogame from './pages/Infogame';
@@ -13,8 +15,7 @@ function AppContent() {
   const [searchResults, setSearchResults] = useState(null);
   const [loadingSearch, setLoadingSearch] = useState(false); 
   const location = useLocation();
-  const hideHeader = location.pathname === "/login" || location.pathname === "/cadastro";
-
+  const hideHeader = location.pathname === "/login" || location.pathname === "/cadastro" || location.pathname === "/forgotpassword" || location.pathname === "/reset-password";
 
   return (
     <>
@@ -41,6 +42,8 @@ function AppContent() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/meus-jogos" element={<MyGames />} />
         <Route path="/infogame" element={<Infogame />} />
       </Routes>
